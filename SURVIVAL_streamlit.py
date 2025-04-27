6f748f6a38af0bdc6f3c6949b3e9782ab27bbfa7
Lakshya Dhaka <lakshyadhaka.987s@gmail.com>
2025-04-27 23:26:10 +0530

    Update SURVIVAL_streamlit.py
    fixed some shi

--- SURVIVAL_streamlit.py
+++ SURVIVAL_streamlit.py
@@ -246,8 +246,12 @@ X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_
 knn = KNeighborsClassifier()
 k_range = list(range(1, 25))
 k_scores = []
+
 for k in k_range:
-knn = KNeighborsClassifier(n_neighbors=k) knn.fit(X_train, y_train) k_scores.append(knn.score(X_train, y_train))
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)
+    k_scores.append(knn.score(X_train, y_train))
+
 print(np.round(k_scores, 4))
 
 
